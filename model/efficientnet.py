95b3f00c65ed30dc17e38a56c96f27bdcb04d7ef
Wang Jiange <2252699@tongji.edu.cn>
2024-10-17 23:27:06 +0800

    update

--- model/efficientnet.py
+++ model/efficientnet.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -18,12 +17,12 @@ from .utils import (
 
 class MBConvBlock(nn.Module):
     """
-    Mobile Inverted Residual Bottleneck Block
-    Args:
-        block_args (namedtuple): BlockArgs, see above
-        global_params (namedtuple): GlobalParam, see above
-    Attributes:
-        has_se (bool): Whether the block contains a Squeeze and Excitation layer.
+    Mobile Inverted Residual Bottleneck Block (MBConv)
+    参数:
+        block_args (namedtuple): BlockArgs
+        global_params (namedtuple): GlobalParam
+    属性:
+        has_se (bool): 该块是否包含Squeeze-and-Excitation层。
     """
 
     def __init__(self, block_args, global_params):
@@ -32,33 +31,33 @@ class MBConvBlock(nn.Module):
         self._bn_mom = 1 - global_params.batch_norm_momentum
         self._bn_eps = global_params.batch_norm_epsilon
         self.has_se = (self._block_args.se_ratio is not None) and (0 < self._block_args.se_ratio <= 1)
-        self.id_skip = block_args.id_skip  # skip connection and drop connect
+        self.id_skip = block_args.id_skip  # 是否使用跳跃连接和drop connect
 
-        # Get static or dynamic convolution depending on image size
+        # 根据图像大小获取静态或动态卷积
         Conv2d = get_same_padding_conv2d(image_size=global_params.image_size)
 
-        # Expansion phase
-        inp = self._block_args.input_filters  # number of input channels
-        oup = self._block_args.input_filters * self._block_args.expand_ratio  # number of output channels
+        # 扩展阶段
+        inp = self._block_args.input_filters  # 输入通道数
+        oup = self._block_args.input_filters * self._block_args.expand_ratio  # 输出通道数
         if self._block_args.expand_ratio != 1:
             self._expand_conv = Conv2d(in_channels=inp, out_channels=oup, kernel_size=1, bias=False)
             self._bn0 = nn.BatchNorm2d(num_features=oup, momentum=self._bn_mom, eps=self._bn_eps)
 
-        # Depthwise convolution phase
+        # 深度卷积阶段
         k = self._block_args.kernel_size
         s = self._block_args.stride
         self._depthwise_conv = Conv2d(
-            in_channels=oup, out_channels=oup, groups=oup,  # groups makes it depthwise
+            in_channels=oup, out_channels=oup, groups=oup,  # groups 参数等于通道数表示深度卷积
             kernel_size=k, stride=s, bias=False)
         self._bn1 = nn.BatchNorm2d(num_features=oup, momentum=self._bn_mom, eps=self._bn_eps)
 
-        # Squeeze and Excitation layer, if desired
+        # Squeeze-and-Excitation 层（如果需要）
         if self.has_se:
             num_squeezed_channels = max(1, int(self._block_args.input_filters * self._block_args.se_ratio))
             self._se_reduce = Conv2d(in_channels=oup, out_channels=num_squeezed_channels, kernel_size=1)
             self._se_expand = Conv2d(in_channels=num_squeezed_channels, out_channels=oup, kernel_size=1)
 
-        # Output phase
+        # 输出阶段
         final_oup = self._block_args.output_filters
         self._project_conv = Conv2d(in_channels=oup, out_channels=final_oup, kernel_size=1, bias=False)
         self._bn2 = nn.BatchNorm2d(num_features=final_oup, momentum=self._bn_mom, eps=self._bn_eps)
@@ -66,18 +65,18 @@ class MBConvBlock(nn.Module):
 
     def forward(self, inputs, drop_connect_rate=None):
         """
-        :param inputs: input tensor
-        :param drop_connect_rate: drop connect rate (float, between 0 and 1)
-        :return: output of block
+        :param inputs: 输入张量
+        :param drop_connect_rate: drop connect 率 (浮点数，范围在 0 到 1 之间)
+        :return: 块的输出
         """
 
-        # Expansion and Depthwise Convolution
+        # 扩展和深度卷积
         x = inputs
         if self._block_args.expand_ratio != 1:
             x = self._swish(self._bn0(self._expand_conv(inputs)))
         x = self._swish(self._bn1(self._depthwise_conv(x)))
 
-        # Squeeze and Excitation
+        # Squeeze-and-Excitation
         if self.has_se:
             x_squeezed = F.adaptive_avg_pool2d(x, 1)
             x_squeezed = self._se_expand(self._swish(self._se_reduce(x_squeezed)))
@@ -85,110 +84,110 @@ class MBConvBlock(nn.Module):
 
         x = self._bn2(self._project_conv(x))
 
-        # Skip connection and drop connect
+        # 跳跃连接和 drop connect
         input_filters, output_filters = self._block_args.input_filters, self._block_args.output_filters
         if self.id_skip and self._block_args.stride == 1 and input_filters == output_filters:
             if drop_connect_rate:
                 x = drop_connect(x, p=drop_connect_rate, training=self.training)
-            x = x + inputs  # skip connection
+            x = x + inputs  # 跳跃连接
         return x
 
     def set_swish(self, memory_efficient=True):
-        """Sets swish function as memory efficient (for training) or standard (for export)"""
+        """设置 Swish 激活函数为内存优化版（用于训练）或标准版（用于导出）"""
         self._swish = MemoryEfficientSwish() if memory_efficient else Swish()
 
 
 class EfficientNet(nn.Module):
     """
-    An EfficientNet model. Most easily loaded with the .from_name or .from_pretrained methods
-    Args:
-        blocks_args (list): A list of BlockArgs to construct blocks
-        global_params (namedtuple): A set of GlobalParams shared between blocks
-    Example:
+    EfficientNet 模型。可以通过 .from_name 或 .from_pretrained 方法加载。
+    参数:
+        blocks_args (list): BlockArgs 列表，用于构建各个块
+        global_params (namedtuple): 全局参数集，在各个块之间共享
+    示例:
         model = EfficientNet.from_pretrained('efficientnet-b0')
     """
 
     def __init__(self, blocks_args=None, global_params=None):
         super().__init__()
-        assert isinstance(blocks_args, list), 'blocks_args should be a list'
-        assert len(blocks_args) > 0, 'block args must be greater than 0'
+        assert isinstance(blocks_args, list), 'blocks_args 应该是一个列表'
+        assert len(blocks_args) > 0, 'block_args 的数量必须大于 0'
         self._global_params = global_params
         self._blocks_args = blocks_args
 
-        # Get static or dynamic convolution depending on image size
+        # 根据图像大小获取静态或动态卷积
         Conv2d = get_same_padding_conv2d(image_size=global_params.image_size)
 
-        # Batch norm parameters
+        # 批标准化参数
         bn_mom = 1 - self._global_params.batch_norm_momentum
         bn_eps = self._global_params.batch_norm_epsilon
 
-        # Stem
-        in_channels = 3  # rgb
-        out_channels = round_filters(32, self._global_params)  # number of output channels
+        # Stem 部分
+        in_channels = 3  # 输入 RGB 图像
+        out_channels = round_filters(32, self._global_params)  # 输出通道数
         self._conv_stem = Conv2d(in_channels, out_channels, kernel_size=3, stride=2, bias=False)
         self._bn0 = nn.BatchNorm2d(num_features=out_channels, momentum=bn_mom, eps=bn_eps)
 
-        # Build blocks
+        # 构建各个块
         self._blocks = nn.ModuleList([])
         for block_args in self._blocks_args:
 
-            # Update block input and output filters based on depth multiplier.
+            # 根据深度乘子更新块的输入和输出滤波器
             block_args = block_args._replace(
                 input_filters=round_filters(block_args.input_filters, self._global_params),
                 output_filters=round_filters(block_args.output_filters, self._global_params),
                 num_repeat=round_repeats(block_args.num_repeat, self._global_params)
             )
 
-            # The first block needs to take care of stride and filter size increase.
+            # 第一个块需要处理步幅和滤波器大小的增加
             self._blocks.append(MBConvBlock(block_args, self._global_params))
             if block_args.num_repeat > 1:
                 block_args = block_args._replace(input_filters=block_args.output_filters, stride=1)
             for _ in range(block_args.num_repeat - 1):
                 self._blocks.append(MBConvBlock(block_args, self._global_params))
 
-        # Head
-        in_channels = block_args.output_filters  # output of final block
+        # Head 部分
+        in_channels = block_args.output_filters  # 最后一个块的输出
         out_channels = round_filters(1280, self._global_params)
         self._conv_head = Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
         self._bn1 = nn.BatchNorm2d(num_features=out_channels, momentum=bn_mom, eps=bn_eps)
 
-        # Final linear layer
+        # 最终全连接层
         self._avg_pooling = nn.AdaptiveAvgPool2d(1)
         self._dropout = nn.Dropout(self._global_params.dropout_rate)
         self._fc = nn.Linear(out_channels, self._global_params.num_classes)
         self._swish = MemoryEfficientSwish()
 
     def set_swish(self, memory_efficient=True):
-        """Sets swish function as memory efficient (for training) or standard (for export)"""
+        """设置 Swish 激活函数为内存优化版（用于训练）或标准版（用于导出）"""
         self._swish = MemoryEfficientSwish() if memory_efficient else Swish()
         for block in self._blocks:
             block.set_swish(memory_efficient)
 
     def extract_features(self, inputs):
-        """ Returns output of the final convolution layer """
+        """ 返回最终卷积层的输出特征 """
 
-        # Stem
+        # Stem 部分
         x = self._swish(self._bn0(self._conv_stem(inputs)))
 
-        # Blocks
+        # 各个块
         for idx, block in enumerate(self._blocks):
             drop_connect_rate = self._global_params.drop_connect_rate
             if drop_connect_rate:
                 drop_connect_rate *= float(idx) / len(self._blocks)
             x = block(x, drop_connect_rate=drop_connect_rate)
 
-        # Head
+        # Head 部分
         x = self._swish(self._bn1(self._conv_head(x)))
 
         return x
 
     def forward(self, inputs):
-        """ Calls extract_features to extract features, applies final linear layer, and returns logits. """
+        """ 调用 extract_features 提取特征，应用最终的全连接层并返回分类结果 """
         bs = inputs.size(0)
-        # Convolution layers
+        # 卷积层
         x = self.extract_features(inputs)
 
-        # Pooling and final linear layer
+        # 全局池化和最终全连接层
         x = self._avg_pooling(x)
         x = x.view(bs, -1)
         x = self._dropout(x)
@@ -219,7 +218,7 @@ class EfficientNet(nn.Module):
 
     @classmethod
     def _check_model_name_is_valid(cls, model_name):
-        """ Validates model name. """
+        """ 验证模型名称是否合法 """
         valid_models = ['efficientnet-b' + str(i) for i in range(9)]
         if model_name not in valid_models:
-            raise ValueError('model_name should be one of: ' + ', '.join(valid_models))
+            raise ValueError('model_name 应该是以下之一: ' + ', '.join(valid_models))
